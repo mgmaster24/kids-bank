29d1e1b5e32b4ef89b020b739c02273bc0ce0845
Michael Masterson <mgmastersonjr@gmail.com>
2024-10-12 18:30:52 -0400

    added account handler trait

--- kids-bank-lib/src/lib.rs
+++ kids-bank-lib/src/lib.rs
@@ -1,9 +1,23 @@
 mod acct_management;
 mod users;
 
-pub fn create_user_account(name: &str, email: &str) -> Result<acct_management::Account, String> {
+use async_trait::async_trait;
+
+pub use acct_management::{Account, AccountError};
+
+pub fn create_user_account(name: &str, email: &str) -> Result<Account, String> {
     let user = users::User::new(name.to_string(), email.to_string());
     Ok(acct_management::Account::new(user))
+}
+
+#[async_trait]
+pub trait AccountHandler {
+    async fn create_account(name: &str, email: &str) -> Result<Account, AccountError>;
+    async fn get_accounts() -> Result<Vec<Account>, AccountError>;
+    async fn get_account_by_id(id: u64) -> Result<Account, AccountError>;
+    async fn get_account_by_email(email: &str) -> Result<Account, AccountError>;
+    async fn withdraw(account_id: u64, amount: f64) -> Result<f64, AccountError>;
+    async fn deposit(account_id: u64, amount: f64) -> Result<f64, AccountError>;
 }
 
 #[cfg(test)]
--- kids-bank-sam/kids-bank-app/src/dynamo_db.rs
+++ kids-bank-sam/kids-bank-app/src/dynamo_db.rs
@@ -1,15 +1,44 @@
+use kids_bank_lib::{self, Account, AccountError, AccountHandler};
+use lambda_http::{http::Error, Body, Response};
+
 #[derive(Debug)]
 struct DynamoClient {
     table_name: String,
     client: aws_sdk_dynamodb::Client,
 }
 
+
 impl DynamoClient {
-    pub fn new(config: &aws_sdk_config::Config, table_name: String) -> Result<Self, String> {
+    pub fn new(config: &aws_config::SdkConfig, table_name: String) -> Result<Self, String> {
         let client = aws_sdk_dynamodb::Client::new(config);
 
         Ok(DynamoClient { table_name, client })
     }
 
-    pub fn query(&self) {}
+    async pub fn get_item(&self) -> Result<Response<Body>, Error> {
+        Err()
+    }
+
+    async pub fn put_item(&self, request: lambda_http::Request) -> Result<Response<Body>, Error> {
+        Err(Error("something happened"))
+    }
+}
+
+impl AccountHandler for DynamoClient {
+    pub fn create_account(name: &str, email: &str) -> Result<Account, AccountError> {
+        let account = kids_bank_lib::create_user_account(name, email);
+        // write account to dynamodb
+    }
+
+    pub fn get_accounts() -> Result<Vec<Account>, AccountError> {
+        Err(AccountError::DoesNotExist)
+    }
+
+    pub fn get_account_by_id(id: u64) -> Result<Account, AccountError> {}
+
+    pub fn get_account_by_email(email: &str) -> Result<Account, AccountError> {}
+
+    pub fn deposit(account_id: u64, amount: f64) -> Result<f64, AccountError> {}
+
+    pub fn withdraw(account_id: u64, amount: f64) -> Result<f64, AccountError> {}
 }
